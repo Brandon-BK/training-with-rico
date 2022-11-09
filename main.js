f9a8dedd7da466b30e936ee452874b4abf692477
Brandon <kisibubrandon5@gmail.com>
2022-11-09 09:16:45 +0200

    template

---
+++ main.js
@@ -0,0 +1,191 @@
+const student = {
+    name : 'Tim',
+    surname : 'Jones',
+    age :  27,
+    testscore : 78,
+    isProgrammer : false,
+}
+
+
+const students = [
+    {
+        name : 'Bob',
+        surname : 'Botombe',
+        age :  21,
+        testscore : 55,
+        isProgrammer : false,
+        roleModels : ['Kanye West', 'Alaxander Forbes', 'Big Sean']
+    },{
+        name : 'Jimmy',
+        surname : 'Hendricks',
+        age :  17,
+        testscore : 44,
+        isProgrammer : true,
+        roleModels : ['Elon Musk', 'Jeff Bezos']
+
+    },{
+        name : 'Josheph',
+        surname : 'Selah',
+        age :  22,
+        testscore : 78,
+        isProgrammer : true,
+        roleModels : ['Warren Beffet', 'Jeff Bezos']
+
+    },{
+        name : 'Borris',
+        surname : 'Johnson',
+        age :  30,
+        testscore : 66,
+        isProgrammer : false,
+        roleModels : ['Jessica Alba', 'Drake']
+
+    },{
+        name : 'Patrick',
+        surname : 'Fatacki',
+        age :  41,
+        testscore : 13,
+        isProgrammer : true,
+        roleModels : ['Mark Zuckerburg', 'Elon Musk']
+
+    },{
+        name : 'Henrico',
+        surname : 'Botes',
+        age :  18,
+        testscore : 97,
+        isProgrammer : false,
+        roleModels : ['Bob Marley', 'Lucky dube']
+
+    },{
+        name : 'Knowledge',
+        surname : 'Mosona',
+        age :  19,
+        testscore : 77,
+        isProgrammer : false,
+        roleModels : ['50 Cent', 'Vibes Cartel']
+
+    },{
+        name : 'Barrick',
+        surname : 'Phillips',
+        age : 23,
+        testscore : 35,
+        isProgrammer : true,
+        roleModels : ['Elon Musk']
+
+    },{
+        name : 'Megan',
+        surname : 'Peters',
+        age :  27,
+        testscore : 88,
+        isProgrammer : false,
+        roleModels : ['Madonna', 'Katty Perry']
+
+    }
+]
+
+
+/**
+ * QUESTION 1.
+ *
+ * using the student variable above return the age of the student.
+ */
+ function getStudentAge() {
+    return 
+  }
+
+  
+  /**
+   * QUESTION 2.
+   *
+   * using the student variable above return the name and surname
+   * of the student as one string (concatination). make sure that the
+   * name is seperated by a space
+   */
+
+  function getStudentFullName() {
+    
+
+
+  }
+
+  
+
+  /**
+   * QUESTION 3.
+   *
+   * Having fun yet 😉 ?
+   *
+   * now using the students variable above return a list containing
+   * the names of all the students who are programers , ie: ['Bob', 'Jeff', 'Tim']
+   *
+   */
+
+
+  function getStudentsWhoAreProgarmers() {
+    
+
+  }
+
+
+
+  
+  /**
+   * QUESTION 4.
+   *
+   * return a list of objects containing a name and score property. The list
+   * should consist of all non programmers who have a testscore hight then 50
+   *
+   * example of the return list:
+   *
+   *  [
+   *    {name : 'Jeff', testscore: 67},
+   *    {name : 'Mary', testscore: 49},
+   *  ]
+   *
+   */
+  function getNonProgramersWithMarkOver50() {
+   
+
+
+  }
+
+  
+
+
+
+  /**
+   * QUESTION 5.
+   *
+   * relax you are doing great 👍 ?
+   *
+   * By having a look at the mockdata we can see for ourselfs
+   * how many students see Elon Musk as a role model. The problem is
+   * that the computer cannot see things like we see them and this is
+   * where you as the programmer step in to teach the computer.
+   *
+   * YOUR TASK :
+   *
+   * Write a function that counts how many students have listed Elon Musk
+   * as a role model, after working out the correct number of students, returns
+   * the count
+   *
+   */
+  function getElonMuskSupporters() {
+    
+  }
+
+
+  /**
+   *  QUESTION 6
+   *  
+   *  take a moment to breath 🧘🏼‍♂️ ...
+   * 
+   *  Given that the pass mark is 70% determine what percentage of students have 
+   *  failed and return the percentage
+   * 
+      NB: The formular for determining the average is :
+      avg = (number of  students who failed / number of all students) * 100
+   */
+  function getPercentageOfStudentsWhoFailed() {
+    
+    
+  }
